7f02cd46a952d83d777c851097876aab7988f38f
Snowdenv20 <55126792+Snowdenv20@users.noreply.github.com>
2020-04-16 22:30:06 -0600

    Menu Principal
    Menu Prinipal como funcion asi mismo la barra de progreso al entrar , declaracion de lineas para cuadros de interfaz grafica con define

--- Proyecto_final/main.cpp
+++ Proyecto_final/main.cpp
@@ -1,4 +1,100 @@
 #include<librerias.h>
-main(){
+char CT = 203, LTI = 204, H = 205, CRUZ = 206, CB = 202, EIA = 201, EIB = 200, LTD = 185, V = 186, EDA = 187, EDB = 188;
+#define ARAR cout << EIA << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<EDA << endl;
+#define AREN cout << LTI << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< LTD << endl;
+#define ARENBJ  cout << LTI  << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<LTD<< endl;
+#define ABJ cout << EIB  << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<EDB<< endl;
+#define arriva cout << LTI << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CT<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<LTD << endl;
+#define separador cout << LTI << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CRUZ<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< LTD << endl;
+#define abajo cout << LTI  << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CB<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<LTD<< endl;
+void Menu();
 
+void Menu(){
+char opc;
+     SetConsoleTitle("Menu Principal");
+     system("CLS");
+         gotoxy(35, 7);
+     ARAR;
+     gotoxy(35, 8);
+     cout << V << "                 Menu Principal                   " << V << endl;
+     gotoxy(35, 9);
+     AREN;
+     gotoxy(35, 10);
+     cout << V << "                 1)Sumar                          " << V << endl;
+     gotoxy(35, 11);
+     cout << V << "                 2)Restar                         " << V << endl;
+     gotoxy(35, 12);
+     cout << V << "                 3)Multiplicar                    " << V << endl;
+     gotoxy(35, 13);
+     cout << V << "                 4)Dividir                        " << V << endl;
+     gotoxy(35, 14);
+     ARENBJ;
+     gotoxy(35, 15);
+     cout << V << "                 ESC.)Salir                       " << V << endl;
+     gotoxy(35, 16);
+     ABJ;
+     gotoxy(35, 17);
+     opc = getch();
+          switch (opc)
+          {
+          case '1':
+          {
+
+               getch();
+               Menu();
+               break;
+          }
+          case '2':
+          {
+              Menu();
+               break;
+          }
+          case '3':
+          {
+        Menu();
+               break;
+          }
+          case '4':
+          {
+        Menu();
+               break;
+          }
+          case 27:{
+          exit(0);
+          break;
+          }
+          default:
+          {
+               cout << "Opcion Incorreta elija de nuevo" << endl;
+               getch();
+              Menu();
+          }
+          }
+     }
+
+main(){
+SetConsoleTitle("RV Machine");
+    system("color 09");
+     cout << "" << endl;
+     cout << "" << endl;
+     cout << "" << endl;
+     cout << "" << endl;
+     cout << "" << endl;
+     cout << "\n\n\n\t\t\t\t             Bienvenidos al Programa \n\n";
+     cout << "\n\t\t\t\t            Hecho por Rodrigo Vasquez\n\n";
+     cout << "\n\t\t\t\t\t\tRV.Enterprices\n\n";
+     char a = 177, b = 219;
+     cout << "\t\t\t\t";
+     for (int i = 0; i <= 50; i++)
+          cout << a;
+     cout << "\r";
+     cout << "\t\t\t\t";
+     for (int i = 0; i <= 50; i++)
+     {
+          cout << b;
+          Sleep(200);
+     }
+     system("CLS");
+
+Menu();
 }
