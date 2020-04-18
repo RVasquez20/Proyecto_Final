8c5128ef6a442a933cca050e518ee9e9ae169e71
Snowdenv20 <55126792+Snowdenv20@users.noreply.github.com>
2020-04-18 17:42:35 -0600

    Marcas
    Implementacion de Ingreso y visualizacion de Listado de marcas(El listado de marcas ya cuenta con Visualizacion Grafica Estandard)

--- Proyecto_final/main.cpp
+++ Proyecto_final/main.cpp
@@ -1,4 +1,5 @@
 #include<librerias.h>
+#include<time.h>
 char CT = 203, LTI = 204, H = 205, CRUZ = 206, CB = 202, EIA = 201, EIB = 200, LTD = 185, V = 186, EDA = 187, EDB = 188;
 #define ARAR cout << EIA << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<EDA << endl;
 #define AREN cout << LTI << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< LTD << endl;
@@ -7,93 +8,51 @@ char CT = 203, LTI = 204, H = 205, CRUZ = 206, CB = 202, EIA = 201, EIB = 200, L
 #define arriva cout << LTI << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CT<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<LTD << endl;
 #define separador cout << LTI << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CRUZ<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< LTD << endl;
 #define abajo cout << LTI  << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CB<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<LTD<< endl;
+
 struct Marcas{
 int idMarca;
 char NombreMarca[50];
 };
+typedef Marcas Marca;
+
 struct Productos{
 int idProducto,idMarca;
 char DescripcionProducto[100];
 double PrecioCosto,PrecioVenta;
 };
+typedef Productos Producto;
+
 struct Clientes{
 int NIT;
 char NombreCliente[50],DireccionCliente[50];
 };
-struct Factura{
+typedef Clientes Cliente;
+
+struct Facturas{
 int NoFactura,NITFactura;
 };
-struct FacturaDetalle{
-int idFacturaDetalle,idProductos,NoFactura;
+typedef Facturas Factura;
+
+struct FacturasDetalle{
+int idProductos,NoFactura;
 double PrecioVenta,Total;
 };
+typedef FacturasDetalle FacturaDetalle;
 
-
-void Menu();
-
-void Menu(){
-char opc;
-Productos Producto;
-     SetConsoleTitle("Menu Principal");
-     system("CLS");
-         gotoxy(35, 7);
-     ARAR;
-     gotoxy(35, 8);
-     cout << V << "                 Menu Principal                   " << V << endl;
-     gotoxy(35, 9);
-     AREN;
-     gotoxy(35, 10);
-     cout << V << "                 1)                         " << V << endl;
-     gotoxy(35, 11);
-     cout << V << "                 2)                     " << V << endl;
-     gotoxy(35, 12);
-     cout << V << "                 3)                       " << V << endl;
-     gotoxy(35, 13);
-     cout << V << "                 4)Dividir                        " << V << endl;
-     gotoxy(35, 14);
-     ARENBJ;
-     gotoxy(35, 15);
-     cout << V << "                 ESC.)Salir                       " << V << endl;
-     gotoxy(35, 16);
-     ABJ;
-     gotoxy(35, 17);
-     opc = getch();
-          switch (opc)
-          {
-          case '1':
-          {
-
-               getch();
-               Menu();
-               break;
-          }
-          case '2':
-          {
-              Menu();
-               break;
-          }
-          case '3':
-          {
-        Menu();
-               break;
-          }
-          case '4':
-          {
-        Menu();
-               break;
-          }
-          case 27:{
-          exit(0);
-          break;
-          }
-          default:
-          {
-               cout << "Opcion Incorreta elija de nuevo" << endl;
-               getch();
-              Menu();
-          }
-          }
-     }
+struct fecha{
+int d,m,a,hh,mm,ss;
+};
+typedef fecha Fecha;
+//Variables Globales para la fecha y hora
+time_t tSac = time(NULL);
+struct tm* tmP = localtime(&tSac);
+char opc;//opciones de funciones del do while
+//Prototipos de Funciones Del Programa
+void MenuPrincipal();
+void MenuMarcas();
+void IngresoDeMarcas();
+void MostrarMarcas();
+void ModificarMarca();
 
 main(){
 /*SetConsoleTitle("WEEKEND PLACE");
@@ -115,5 +74,222 @@ main(){
      }
      system("CLS");
 */
-Menu();
+MenuPrincipal();
 }
+void MenuPrincipal(){
+char opc;
+     SetConsoleTitle("Menu Principal");
+     system("CLS");
+         gotoxy(35, 7);
+     ARAR;
+     gotoxy(35, 8);
+     cout << V << "                 Menu Principal                   " << V << endl;
+     gotoxy(35, 9);
+     AREN;
+     gotoxy(35, 10);
+     cout << V << "                  1. Marcas                       " << V << endl;
+     gotoxy(35, 11);
+     cout << V << "                  2. Inventario                   " << V << endl;
+     gotoxy(35, 12);
+     cout << V << "                  3. Factura                      " << V << endl;
+     gotoxy(35, 13);
+     cout << V << "                  4. Pedidos                      " << V << endl;
+     gotoxy(35, 14);
+     ARENBJ;
+     gotoxy(35, 15);
+     cout << V << "                  ESC. Salir                      " << V << endl;
+     gotoxy(35, 16);
+     ABJ;
+     gotoxy(35, 17);
+     opc = getch();
+          switch (opc)
+          {
+          case '1':
+          {
+               MenuMarcas();
+               getch();
+               MenuPrincipal();
+               break;
+          }
+          case '2':
+          {
+              MenuPrincipal();
+               break;
+          }
+          case '3':
+          {
+        MenuPrincipal();
+               break;
+          }
+          case '4':
+          {
+        MenuPrincipal();
+               break;
+          }
+          case 27:{
+          exit(0);
+          break;
+          }
+          default:
+          {
+               cout << "Opcion Incorreta elija de nuevo" << endl;
+               getch();
+              MenuPrincipal();
+          }
+          }
+     }
+void MenuMarcas(){
+char opc;
+     SetConsoleTitle("Menu Marcas");
+     system("CLS");
+         gotoxy(35, 7);
+     ARAR;
+     gotoxy(35, 8);
+     cout << V << "               Menu Principal Marcas              " << V << endl;
+     gotoxy(35, 9);
+     AREN;
+     gotoxy(35, 10);
+     cout << V << "              1. Ingresar Marcas                  " << V << endl;
+     gotoxy(35, 11);
+     cout << V << "              2. Mostrar Listado de Marcas        " << V << endl;
+     gotoxy(35, 12);
+     cout << V << "              3. Modificar Marca                  " << V << endl;
+     gotoxy(35, 13);
+     cout << V << "              4. Eliminar Marca                   " << V << endl;
+     gotoxy(35, 14);
+     ARENBJ;
+     gotoxy(35, 15);
+     cout << V << "                  <-- Regresar                    " << V << endl;
+     gotoxy(35, 16);
+     ABJ;
+     gotoxy(35, 17);
+     opc = getch();
+      if (opc == 8)
+     {
+          MenuPrincipal();
+     }
+     else
+     {
+          switch (opc)
+          {
+          case '1':
+          {
+               IngresoDeMarcas();
+               getch();
+               MenuMarcas();
+               break;
+          }
+          case '2':
+          {
+              MostrarMarcas();
+              getch();
+              MenuMarcas();
+               break;
+          }
+          case '3':
+          {
+          ModificarMarca();
+          getch();
+          MenuMarcas();
+               break;
+          }
+          case '4':
+          {
+        MenuMarcas();
+               break;
+          }
+          case 27:{
+          exit(0);
+          break;
+          }
+          default:
+          {
+               cout << "Opcion Incorreta elija de nuevo" << endl;
+               getch();
+              MenuMarcas();
+          }
+          }
+     }
+
+
+}
+void IngresoDeMarcas(){
+     fflush(stdin);
+     Marcas Marca;
+     FILE *ArchivoMarcas=fopen("Marcas.dat","a+b");
+     loop:
+     rewind(ArchivoMarcas);
+     int AuxIdMarca;
+     system("CLS");
+     fflush(stdin);
+     cout<<"Ingrese el Id de la marca"<<endl;
+     cin>>AuxIdMarca;
+     fread(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+          while(!feof(ArchivoMarcas)){
+          if(AuxIdMarca==Marca.idMarca){
+          cout<<"Id Ya Existente"<<endl;
+          getch();
+          goto loop;
+          }
+          fread(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+          }
+          fflush(stdin);
+          Marca.idMarca=AuxIdMarca;
+     cout<<"Ingrese El Nombre De La Marca"<<endl;
+     cin.getline(Marca.NombreMarca,50,'\n');
+     fflush(stdin);
+     fwrite(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+     cout<<"Guardado Exitosamente"<<endl;
+     fclose(ArchivoMarcas);
+}
+void MostrarMarcas(){
+system("CLS");
+int Filas=6,FilasTotales=6;
+SetConsoleTitle("Listado De Marcas");
+Marcas Marca;
+FILE *ArchivoMarcas=fopen("Marcas.dat","rb");
+system("CLS");
+fread(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+gotoxy(10,3);cout << EIA << H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CT<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<EDA << endl;
+gotoxy(10,4);cout<<V;gotoxy(30,4);cout<<"Id Marca";gotoxy(59,4);cout<<V;gotoxy(80,4);cout<<"Nombre";gotoxy(110,4);cout<<V<<endl;
+gotoxy(10,5);separador;
+
+     while(!feof(ArchivoMarcas)){
+     gotoxy(10,Filas);cout<<V;gotoxy(32,Filas);cout<<Marca.idMarca;gotoxy(59,Filas);cout<<V;gotoxy(82,Filas);cout<<Marca.NombreMarca;gotoxy(110,Filas);cout<<V<<endl;
+    ++Filas;
+    ++FilasTotales;
+    fread(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+     }
+gotoxy(10,FilasTotales);cout << EIB<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<CB<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<<H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<< H<<H<< H<<EDB << endl;;
+fclose(ArchivoMarcas);
+}
+void ModificarMarca(){
+fflush(stdin);
+     Marcas Marca;
+     FILE *ArchivoMarcas=fopen("Marcas.dat","a+b");
+     loop:
+     rewind(ArchivoMarcas);
+     int AuxIdMarca;
+     system("CLS");
+     fflush(stdin);
+     cout<<"Ingrese el Id de la marca"<<endl;
+     cin>>AuxIdMarca;
+     fread(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+          while(!feof(ArchivoMarcas)){
+          if(AuxIdMarca==Marca.idMarca){
+          cout<<"Id Ya Existente"<<endl;
+          getch();
+          goto loop;
+          }
+          fread(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+          }
+          fflush(stdin);
+          Marca.idMarca=AuxIdMarca;
+     cout<<"Ingrese El Nombre De La Marca"<<endl;
+     cin.getline(Marca.NombreMarca,50,'\n');
+     fflush(stdin);
+     fwrite(&Marca,sizeof(Marcas),1,ArchivoMarcas);
+     cout<<"Guardado Exitosamente"<<endl;
+     fclose(ArchivoMarcas);
+}
+
